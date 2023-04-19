375c71255e126080b33e9623985b15f40381fbc5
Rent09 <darktjedi@rambler.ru>
2023-04-19 21:57:31 +0600

    fix: php telegram

---
+++ handler.php
@@ -0,0 +1,25 @@
+<?php
+
+$user_name = htmlspecialchars($_POST['username']);
+$user_phone = htmlspecialchars($_POST['userphone']);
+
+$token = "6014777932:AAFidmIdAUX2GarByYviLV5qReGQ85Hw0pQ";
+$chat_id = "-919390717";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
+
