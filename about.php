7ef1ffdd62e48299209aed817d72ceccd6052444
Rent09 <darktjedi@rambler.ru>
2023-04-27 00:56:53 +0600

    fix: modal window refactoring

--- about.php
+++ about.php
@@ -121,7 +121,7 @@
           >+7 (499) 686-10-14</a
         >
       </div>
-      <button class="navbar-button button" data-toggle="modal">
+      <button class="navbar-button button" data-toggle="modal" data-target="#feedback-modal">
         <svg class="button-icon" width="24" height="24">
           <use href="img/sprite.svg#phone"></use>
         </svg>
